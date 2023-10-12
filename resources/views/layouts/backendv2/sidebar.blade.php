676fa212a360fa144c24421d1126382de27eb508
OscarMan24 <castellanooscar26@gmail.com>
2023-10-11 20:06:55 -0500

    Agregar opciones del sidebar para los puntos de ventas

--- resources/views/layouts/backendv2/sidebar.blade.php
+++ resources/views/layouts/backendv2/sidebar.blade.php
@@ -3,18 +3,23 @@
         <li class="sidebar-title">
             {{ __('Principal') }}
         </li>
-        @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('Superadmin'))
-             <li class="{{ (request()->is('eventos*')) ? 'active-page' : '' }}" >
-                <a href="{{ route('index.eventos') }}" class=""><i data-feather="calendar"></i>{{ __('Admin eventos') }}</a>
+        @if (Auth::user()->hasRole('admin') || Auth::user()->hasRole('Superadmin'))
+            <li class="{{ request()->is('eventos*') ? 'active-page' : '' }}">
+                <a href="{{ route('index.eventos') }}" class=""><i
+                        data-feather="calendar"></i>{{ __('Admin eventos') }}</a>
             </li>
         @endif
 
-        @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('Superadmin') || Auth::user()->hasRole('organization'))
+        @if (Auth::user()->hasRole('admin') ||
+                Auth::user()->hasRole('Superadmin') ||
+                Auth::user()->hasRole('organization') ||
+                Auth::user()->hasRole('punto venta'))
             <li class="active-page open">
                 <a href="#" class="active">
-                    <i data-feather="folder"></i>{{ __('Mis entradas') }}<i class="fas fa-chevron-right dropdown-icon"></i>
+                    <i data-feather="folder"></i>{{ __('Mis entradas') }}<i
+                        class="fas fa-chevron-right dropdown-icon"></i>
                 </a>
-                <ul class="">                    
+                <ul class="">
                     @livewire('misentradas.nuevo.reporte-livewire')
                 </ul>
             </li>
