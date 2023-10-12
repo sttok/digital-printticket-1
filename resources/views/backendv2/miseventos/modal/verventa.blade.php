22fd263287a043d7c194c989e7543b8263d0f5df
OscarMan24 <castellanooscar26@gmail.com>
2023-10-11 21:23:02 -0500

    Test de comprobacion de validacion

--- resources/views/backendv2/miseventos/modal/verventa.blade.php
+++ resources/views/backendv2/miseventos/modal/verventa.blade.php
@@ -72,7 +72,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-
+                <p>{{ $estadoVenta . ' ... ' . $estadoAnulacion }}</p>
                 @if ($estadoVenta == 1 && $estadoAnulacion == 0)
                     <button type="button" class="btn btn-danger float-left" wire:loading.attr="disabled"
                         wire:click="solicitarAnulacion()">
