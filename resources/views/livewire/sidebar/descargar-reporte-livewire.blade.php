63e22e1d1f0e3579f57bd9fed6b6b9e88864d284
OscarMan24 <castellanooscar26@gmail.com>
2023-10-11 19:35:01 -0500

    Descargar Reporte por puntos de ventas

--- resources/views/livewire/sidebar/descargar-reporte-livewire.blade.php
+++ resources/views/livewire/sidebar/descargar-reporte-livewire.blade.php
@@ -1,68 +1,68 @@
 <div wire:init="loadDatos">
-    @if($readyToLoad2)
+    @if ($readyToLoad2)
         <script>
-         window.addEventListener('downloadReport', event => {
-            Swal.fire({
-                title: '¿Que reporte desea descargar?',
-                icon: 'question',
-                showDenyButton: true,
-                showCancelButton: true,
-                confirmButtonText: 'Todos',
-                denyButtonText: `Filtrar por organizador`,
-                cancelButtonText: 'Cancelar'
-            }).then((result) => {
-                /* Read more about isConfirmed, isDenied below */
-                if (result.isConfirmed) {
-                    window.livewire.emit('downloadReportesAll')
-                    let timerInterval
-                    Swal.fire({
-                        icon: 'success',
-                        title: '¡Procesando! ',
-                        text: 'Espera un momento, pronto estará disponible',
-                        timer: 1500,
-                        timerProgressBar: true,
-                        didOpen: () => {
-                            Swal.showLoading()
-                        },
-                        willClose: () => {
-                            clearInterval(timerInterval)
-                        }
-                    })
-                }else if (result.isDenied) {
-                   Swal.fire({
-                        title: 'Selecciona un punto de venta',
-                        icon: 'info',
-                        input: 'select',
-                        inputOptions: @json($dataPuntosVentas),
-                        inputPlaceholder: 'Selecciona un punto de venta',
-                        showCancelButton: true,
-                        confirmButtonText: 'Seleccionar',
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            const selectedPromoterId = result.value;
-                            window.livewire.emit('downloadReportesPuntoVenta', selectedPromoterId);
-                            let timerInterval
-                            Swal.fire({
-                                icon: 'success',
-                                title: '¡Procesando! ',
-                                text: 'Espera un momento, pronto estará disponible',
-                                timer: 1500,
-                                timerProgressBar: true,
-                                didOpen: () => {
-                                    Swal.showLoading()
-                                },
-                                willClose: () => {
-                                    clearInterval(timerInterval)
-                                }
-                            });
-                        }
-                    });
-                }
+            window.addEventListener('downloadReport', event => {
+                Swal.fire({
+                    title: '¿Que reporte desea descargar?',
+                    icon: 'question',
+                    showDenyButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Todos',
+                    denyButtonText: `Filtrar por puntos de ventas`,
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                        window.livewire.emit('downloadReportesAll')
+                        let timerInterval
+                        Swal.fire({
+                            icon: 'success',
+                            title: '¡Procesando! ',
+                            text: 'Espera un momento, pronto estará disponible',
+                            timer: 1500,
+                            timerProgressBar: true,
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            willClose: () => {
+                                clearInterval(timerInterval)
+                            }
+                        })
+                    } else if (result.isDenied) {
+                        Swal.fire({
+                            title: 'Selecciona un punto de venta',
+                            icon: 'info',
+                            input: 'select',
+                            inputOptions: @json($dataPuntosVentas),
+                            inputPlaceholder: 'Selecciona un punto de venta',
+                            showCancelButton: true,
+                            confirmButtonText: 'Seleccionar',
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                const selectedPromoterId = result.value;
+                                window.livewire.emit('downloadReportesPuntoVenta', selectedPromoterId);
+                                let timerInterval
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: '¡Procesando! ',
+                                    text: 'Espera un momento, pronto estará disponible',
+                                    timer: 1500,
+                                    timerProgressBar: true,
+                                    didOpen: () => {
+                                        Swal.showLoading()
+                                    },
+                                    willClose: () => {
+                                        clearInterval(timerInterval)
+                                    }
+                                });
+                            }
+                        });
+                    }
+                })
             })
-        })
-    </script>
+        </script>
     @endif
-    
+
     <!--<script>
         const downloadReport = document.getElementById("downloadReport");
         downloadReport.addEventListener('click', event => {
@@ -127,5 +127,5 @@
             })
         })
     </script>-->
-      
+
 </div>
